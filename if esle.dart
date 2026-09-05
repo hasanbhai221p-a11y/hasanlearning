c655e13b77be017c8d7f8d2506572cfc5f7d88d2
hasanbhai221p-a11y <hasanbhai221p@gmail.com>
2026-09-05 09:43:06 +0500

    hasan.dart & if esle.dart

---
+++ if esle.dart
@@ -0,0 +1,14 @@
+void main() {
+  int num1 = 13;
+  int num2 = 13;
+  if (num2 > num1) {
+    print('num2 is greater than num1');
+  } else if (num1 == num2) {
+    print('num1 is equal to num2');
+  } else {
+    print('num1 is greater than num2');
+  }
+  print('hasan ustaad no1 in the world');
+}
+
+void main2() {}
